cc64774e0101ca36e7431155233bd3cc234ee811
Vinnicyus Carvalho <vcgoncalves@stefanini.com>
2023-03-13 23:26:33 -0300

    refactor: service prisma

--- code/src/modules/users/users.module.ts
+++ code/src/modules/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../../services/prisma/prisma.service';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { PrismaModule } from 'src/services/prisma/prisma.module';
 
 @Module({
-  providers: [UsersResolver, UsersService, PrismaService],
+  imports: [PrismaModule],
+  providers: [UsersResolver, UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
