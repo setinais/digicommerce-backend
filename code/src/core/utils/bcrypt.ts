61e39d3f7db182d9f2b3eae89c35277c5fffed0c
Vinnicyus Carvalho <vcgoncalves@stefanini.com>
2023-10-14 21:24:49 -0300

    revert documents

--- code/src/core/utils/bcrypt.ts
+++ code/src/core/utils/bcrypt.ts
@@ -1,5 +1,9 @@
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 export const genPassword = (password: string): string => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+export const compare = (password: string, userPassword: string): boolean => {
+  return bcrypt.compareSync(password, userPassword);
 };
--- code/src/prisma/seed.ts
+++ code/src/prisma/seed.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { users } from './seeds/users';
 
 async function main() {
   const prisma = new PrismaClient({ log: ['query'], errorFormat: 'pretty' });
@@ -21,6 +22,10 @@ async function main() {
     // }
     // const next = await prisma.profile.count();
     // await prisma.$executeRaw`SELECT setval('profiles_id_seq', ${next}, true);`;
+    for (const user of users) {
+      const created = await prisma.user.create({ data: user });
+      // Logger.debug(created, `Upsert profile: ${profile.id}`);
+    }
   } catch (e) {
     Logger.error(e);
     process.exit(1);
